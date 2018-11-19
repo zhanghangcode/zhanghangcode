19948b7ed161d6a5814338adf4de30ae2dab8fd3
zhanghang <code_legend@163.com>
2018-11-19 18:58:14 +0900

    使用dubbo将服务层和web层分开

--- common-manager/common-manager-interface/src/main/java/com/min/service/ItemService.java
+++ common-manager/common-manager-interface/src/main/java/com/min/service/ItemService.java
@@ -1,7 +1,9 @@
 package com.min.service;
 
+import com.min.common.pojo.EasyUIDataGridResult;
 import com.min.pojo.TbItem;
 
-public interface ItemService {
+public interface ItemService<T> {
     TbItem getItemById(long itemId);
+    EasyUIDataGridResult<T> getItemList(int page,int rows);
 }
--- common-manager/common-manager-service/src/main/java/com/min/service/impl/ItemServiceimpl.java
+++ common-manager/common-manager-service/src/main/java/com/min/service/impl/ItemServiceimpl.java
@@ -1,10 +1,17 @@
 package com.min.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.min.common.pojo.EasyUIDataGridResult;
 import com.min.mapper.TbItemMapper;
 import com.min.pojo.TbItem;
+import com.min.pojo.TbItemExample;
 import com.min.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ItemServiceimpl implements ItemService {
@@ -16,5 +23,24 @@ public class ItemServiceimpl implements ItemService {
     public TbItem getItemById(long itemId) {
         TbItem tbItem = tbItemMapper.selectByPrimaryKey(itemId);
         return tbItem;
+    }
+
+    @Override
+    public EasyUIDataGridResult<Page> getItemList(int page, int rows) {
+        //设置分页信息
+        PageHelper.startPage(page,rows);
+        //执行查询
+        TbItemExample tbItemExample=new TbItemExample();
+        List<TbItem> tbItems = tbItemMapper.selectByExample(tbItemExample);
+        //创建一个返回值对象
+        EasyUIDataGridResult<Page> result=new EasyUIDataGridResult<>();
+        result.setRows((Page)tbItems);
+        //分页结果
+        PageInfo<TbItem> pageInfo=new PageInfo<>(tbItems);
+        //获取总记录数
+        long total = pageInfo.getTotal();
+        result.setTotal(total);
+
+        return result;
     }
 }
